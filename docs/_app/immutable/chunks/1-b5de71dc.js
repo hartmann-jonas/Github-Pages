4424c64652cbe77b3227a778494fa7a20f368e1f
Jonas Hartmann <80170229+hartmann-jonas@users.noreply.github.com>
2023-04-03 13:12:12 +0200

    npm run build

---
+++ docs/_app/immutable/chunks/1-b5de71dc.js
@@ -0,0 +1,47 @@
+import{S as Je,i as Ke,s as Ne,k as i,q as o,a as v,l as a,m as p,r as l,h as s,c as d,n as b,b as h,G as t,B as Re}from"./index-b0905f39.js";function Oe(Ue){let w,J,V,u,K,E,N,O,x,Q,X,T,Y,Z,B,$,g,A,I,ee,y,n,te,se,oe,le,ie,ae,re,ne,he,fe,pe,ue,me,ve,de,ce,be,C,k,we,F,M,_e,q,m,S,Ee,xe,H,Te,Be,L,De,Ie,P,ke,z,R,Me,G,D,We=`<code class="language-undefined">---
+title: Version x.x.x
+date: &quot;2023-04-03&quot;
+---
+# Short Title
+
+## Description
+Some descriptive text that gives a quick
+overview on the changes made.
+
+## Changes
+More detailed desctiption of the changes:
+1. Fetch function to get this
+2. Restructuring the navbar to improve 
+3.
+
+Some more descriptive text about changes in detail
+
+## Features
+✅ Cool new feature ✨  
+✅ Another mediocre feature 🎉  
+
+&lt;hr&gt;
+&lt;br&gt;
+Commit: [Version x.x.x](link to commit)
+
+Published: 2023-xx-xx</code>`,U,W,j;return{c(){w=i("h2"),J=o("How to develop a Movie Website"),V=v(),u=i("p"),K=o("This blog is about the development and concept of this "),E=i("a"),N=o("Movie Database"),O=o(` website.
+The website is hosted on `),x=i("a"),Q=o("Vercel"),X=o(" and was developed using "),T=i("a"),Y=o("Svelte"),Z=o(" as well as the "),B=i("a"),$=o("TheMovieDB API"),g=o("."),A=v(),I=i("h2"),ee=o("Features"),y=v(),n=i("p"),te=o("The website has the following features:"),se=i("br"),oe=o(`
+✅ Daily popular movies`),le=i("br"),ie=o(`
+✅ Search movie by name`),ae=i("br"),re=o(`
+✅ User login`),ne=i("br"),he=o(`
+✅ Like movies`),fe=i("br"),pe=o(`
+✅ Save movies`),ue=i("br"),me=o(`
+✅ Individualized recommendations`),ve=i("br"),de=o(`
+❌ More optimized recommendations`),ce=i("br"),be=o(`
+❌ Links to watch the movie on the movie providers website`),C=v(),k=i("h2"),we=o("Documentation"),F=v(),M=i("p"),_e=o("How do I document the process developing that website? I set myself some rules to keep each update consistant."),q=v(),m=i("ul"),S=i("li"),Ee=o("This post gets updated each time a news feature is introduced"),xe=v(),H=i("li"),Te=o("This post serves as documentation on how to use the site"),Be=v(),L=i("li"),De=o("Each “Version” post describes the changes that were made as well as new features and includes a link to the GitHub commit"),Ie=v(),P=i("li"),ke=o("A “Version” post consists of the following sections: Description, Changes, Features"),z=v(),R=i("p"),Me=o("The “Version” posts follow this template:"),G=v(),D=i("pre"),U=v(),W=i("br"),j=o(`
+Published: 2023-04-03`),this.h()},l(e){w=a(e,"H2",{});var r=p(w);J=l(r,"How to develop a Movie Website"),r.forEach(s),V=d(e),u=a(e,"P",{});var c=p(u);K=l(c,"This blog is about the development and concept of this "),E=a(c,"A",{href:!0,rel:!0});var Se=p(E);N=l(Se,"Movie Database"),Se.forEach(s),O=l(c,` website.
+The website is hosted on `),x=a(c,"A",{href:!0,rel:!0});var He=p(x);Q=l(He,"Vercel"),He.forEach(s),X=l(c," and was developed using "),T=a(c,"A",{href:!0,rel:!0});var Le=p(T);Y=l(Le,"Svelte"),Le.forEach(s),Z=l(c," as well as the "),B=a(c,"A",{href:!0,rel:!0});var Pe=p(B);$=l(Pe,"TheMovieDB API"),Pe.forEach(s),g=l(c,"."),c.forEach(s),A=d(e),I=a(e,"H2",{});var Ve=p(I);ee=l(Ve,"Features"),Ve.forEach(s),y=d(e),n=a(e,"P",{});var f=p(n);te=l(f,"The website has the following features:"),se=a(f,"BR",{}),oe=l(f,`
+✅ Daily popular movies`),le=a(f,"BR",{}),ie=l(f,`
+✅ Search movie by name`),ae=a(f,"BR",{}),re=l(f,`
+✅ User login`),ne=a(f,"BR",{}),he=l(f,`
+✅ Like movies`),fe=a(f,"BR",{}),pe=l(f,`
+✅ Save movies`),ue=a(f,"BR",{}),me=l(f,`
+✅ Individualized recommendations`),ve=a(f,"BR",{}),de=l(f,`
+❌ More optimized recommendations`),ce=a(f,"BR",{}),be=l(f,`
+❌ Links to watch the movie on the movie providers website`),f.forEach(s),C=d(e),k=a(e,"H2",{});var Ae=p(k);we=l(Ae,"Documentation"),Ae.forEach(s),F=d(e),M=a(e,"P",{});var ye=p(M);_e=l(ye,"How do I document the process developing that website? I set myself some rules to keep each update consistant."),ye.forEach(s),q=d(e),m=a(e,"UL",{});var _=p(m);S=a(_,"LI",{});var Ce=p(S);Ee=l(Ce,"This post gets updated each time a news feature is introduced"),Ce.forEach(s),xe=d(_),H=a(_,"LI",{});var Fe=p(H);Te=l(Fe,"This post serves as documentation on how to use the site"),Fe.forEach(s),Be=d(_),L=a(_,"LI",{});var qe=p(L);De=l(qe,"Each “Version” post describes the changes that were made as well as new features and includes a link to the GitHub commit"),qe.forEach(s),Ie=d(_),P=a(_,"LI",{});var ze=p(P);ke=l(ze,"A “Version” post consists of the following sections: Description, Changes, Features"),ze.forEach(s),_.forEach(s),z=d(e),R=a(e,"P",{});var Ge=p(R);Me=l(Ge,"The “Version” posts follow this template:"),Ge.forEach(s),G=d(e),D=a(e,"PRE",{class:!0});var je=p(D);je.forEach(s),U=d(e),W=a(e,"BR",{}),j=l(e,`
+Published: 2023-04-03`),this.h()},h(){b(E,"href","https://movie-db-two-hartmann-jonas.vercel.app"),b(E,"rel","nofollow"),b(x,"href","https://vercel.com"),b(x,"rel","nofollow"),b(T,"href","https://svelte.dev"),b(T,"rel","nofollow"),b(B,"href","https://developers.themoviedb.org/3"),b(B,"rel","nofollow"),b(D,"class","language-undefined")},m(e,r){h(e,w,r),t(w,J),h(e,V,r),h(e,u,r),t(u,K),t(u,E),t(E,N),t(u,O),t(u,x),t(x,Q),t(u,X),t(u,T),t(T,Y),t(u,Z),t(u,B),t(B,$),t(u,g),h(e,A,r),h(e,I,r),t(I,ee),h(e,y,r),h(e,n,r),t(n,te),t(n,se),t(n,oe),t(n,le),t(n,ie),t(n,ae),t(n,re),t(n,ne),t(n,he),t(n,fe),t(n,pe),t(n,ue),t(n,me),t(n,ve),t(n,de),t(n,ce),t(n,be),h(e,C,r),h(e,k,r),t(k,we),h(e,F,r),h(e,M,r),t(M,_e),h(e,q,r),h(e,m,r),t(m,S),t(S,Ee),t(m,xe),t(m,H),t(H,Te),t(m,Be),t(m,L),t(L,De),t(m,Ie),t(m,P),t(P,ke),h(e,z,r),h(e,R,r),t(R,Me),h(e,G,r),h(e,D,r),D.innerHTML=We,h(e,U,r),h(e,W,r),h(e,j,r)},p:Re,i:Re,o:Re,d(e){e&&s(w),e&&s(V),e&&s(u),e&&s(A),e&&s(I),e&&s(y),e&&s(n),e&&s(C),e&&s(k),e&&s(F),e&&s(M),e&&s(q),e&&s(m),e&&s(z),e&&s(R),e&&s(G),e&&s(D),e&&s(U),e&&s(W),e&&s(j)}}}const Xe={title:"Development of a Movie Website",date:"2023-04-03"};class Ye extends Je{constructor(w){super(),Ke(this,w,null,Oe,Ne,{})}}export{Ye as default,Xe as metadata};
--- docs/_app/immutable/modules/pages/blog/_path_/_page.js-f53fbb4c.js
+++
@@ -1,1 +0,0 @@
-import"../../../../chunks/preload-helper-41c905a7.js";import{l}from"../../../../chunks/_page-69e2a91c.js";export{l as load};
